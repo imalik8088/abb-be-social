725513c072fcb873cf5898d64ae55076a6899dcb
simPm <dimitros.23@gmail.com>
2013-11-21 13:56:46 +0100

    Service added

---
+++ ABBConnect.Service/ServerService/ServerService/IDALService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ServerService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    [ServiceKnownType(typeof(SqlParameter[]))]
+    [ServiceKnownType(typeof(object))]
+    [ServiceKnownType(typeof(SqlDbType))]
+    [ServiceKnownType(typeof(ParameterDirection))]
+    //[ServiceKnownType(typeof(SqlDateTime))]
+    public interface IDALService
+    {
+        [OperationContract]
+        DataSet ExecuteDataSet(string commandText, SQLParameter[] dsparams);
+
+        [OperationContract]
+        int ExecuteNonQueryOutput(string commandText, SQLParameter[] nonparams, ref object output);
+
+        [OperationContract]
+        int ExecuteNonQuery(string commandText, SQLParameter[] nonparams);
+
+        [OperationContract]
+        object ExecuteScalar(string commandText, SQLParameter[] nonparams);
+    }
+}
