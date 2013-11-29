a5390c0dbfd0a8a222f8757a16b3b9bfdf49ca90
Robert Gustavsson <rgn09003@student.mdh.se>
2013-11-29 08:52:08 +0100

    Made all methods async

--- ABBConnect.PortableTL/PortableTransformationLayer/PortableTransformationLayer/Accesser.cs
+++ ABBConnect.PortableTL/PortableTransformationLayer/PortableTransformationLayer/Accesser.cs
@@ -10,6 +10,20 @@ using PortableTransformationLayer.ABBConnectServiceRef;
 using System.Threading.Tasks;
 
 
+/*
+ * Connects to the JSON service hosted at remote server
+ * Get the results in a async way.
+ * IS used by Windows phoneetc.
+ * 
+ * Written by: Robert G
+ * 2013-11-29
+ * 
+ * STUFF THAT NEEDS TO BE ADDED:
+ * Fault codes when reciveing data
+ * More methods
+ * 
+ */
+
 namespace PortableTransformationLayer
 {
     public class Accesser
@@ -26,16 +40,24 @@ namespace PortableTransformationLayer
 
         }
 
-        public List<GetPriorityCategories_Result> GetCategories()
+        /// <summary>
+        /// Get all categories
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<GetPriorityCategories_Result>> GetCategories()
         {
             
             var client = new HttpClient();
             client.BaseAddress = new Uri(url);
-            var response = client.GetAsync("GetCategories").Result;
-            var result = response.Content.ReadAsStringAsync().Result;
-            return JsonConvert.DeserializeObject<List<GetPriorityCategories_Result>>(result);
+            var response = await client.GetStringAsync("GetCategories").ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<List<GetPriorityCategories_Result>>(response);
         }
 
+        /// <summary>
+        /// Get the lastest feeds, X is the number of feeds
+        /// </summary>
+        /// <param name="X"></param>
+        /// <returns></returns>
         public async Task<List<GetLatestXFeeds_Result>> GetLatestXFeeds(int X)
         {
 
@@ -51,72 +73,103 @@ namespace PortableTransformationLayer
         }
 
 
- 
+        /// <summary>
+        /// Get the lastest feeds from specific Id, X is number of feeds
+        /// </summary>
+        /// <param name="X"></param>
+        /// <param name="Id"></param>
+        /// <returns></returns>
         public async Task<List<GetLatestXFeeds_Result>> GetLatestXFeedsFromId(int X, int Id)
         {
             
             var client = new HttpClient();
             client.BaseAddress = new Uri(url);
-            var response = await client.GetStringAsync("GetLatestXFeeds/" + X.ToString() + "&" + Id.ToString());
- 
-         //   var result =  response.Content.ReadAsStringAsync().Result;
-            return JsonConvert.DeserializeObject<List<GetLatestXFeeds_Result>>(response);
-            
+            var response = await client.GetStringAsync("GetLatestXFeeds/" + X.ToString() + "&" + Id.ToString()).ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<List<GetLatestXFeeds_Result>>(response);       
         }
 
-
-            
-        public bool LogIn(string usrName, string pw)
+        /// <summary>
+        /// Log a user in to the system
+        /// </summary>
+        /// <param name="usrName"></param>
+        /// <param name="pw"></param>
+        /// <returns></returns>
+ 
+        public async Task<bool> LogIn(string usrName, string pw)
         {
             var client = new HttpClient();
             client.BaseAddress = new Uri(url);
-            var response = client.GetAsync("LogIn/" + usrName + "&" + pw).Result;
-            var result = response.Content.ReadAsStringAsync().Result;
-            return JsonConvert.DeserializeObject<bool>(result);
+            var response = await client.GetStringAsync("LogIn/" + usrName + "&" + pw).ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<bool>(response);
         
         }
 
-        public GetHumanInformation_Result GetHumanInformation(int Id)
+        /// <summary>
+        /// The information about a user from his Id
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public async Task<GetHumanInformation_Result> GetHumanInformation(int Id)
         {
             var client = new HttpClient();
             client.BaseAddress = new Uri(url);
-            var response = client.GetAsync("GetHumanInfo/" + Id.ToString()).Result;
-            var result = response.Content.ReadAsStringAsync().Result;
-            return JsonConvert.DeserializeObject<GetHumanInformation_Result>(result);
+            var response = await client.GetStringAsync("GetHumanInfo/" + Id.ToString()).ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<GetHumanInformation_Result>(response);
         }
 
-        public GetHumanInformationByUsername_Result GetHumanInformationByUserName(string username)
+        /// <summary>
+        ///  The information about a user from his username
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns></returns>
+        public async Task<GetHumanInformationByUsername_Result> GetHumanInformationByUserName(string username)
         {
             var client = new HttpClient();
             client.BaseAddress = new Uri(url);
-            var response = client.GetAsync("GetHumanInfoByUsername/" + username).Result;
-            var result = response.Content.ReadAsStringAsync().Result;
-            return JsonConvert.DeserializeObject<GetHumanInformationByUsername_Result>(result);
-        }
-        public List<GetFeedComments_Result> GetFeedComments(int feedId)
-        {
-            var client = new HttpClient();
-            client.BaseAddress = new Uri(url);
-            var response = client.GetAsync("GetFeedComments/" + feedId).Result;
-            var result = response.Content.ReadAsStringAsync().Result;
-            return JsonConvert.DeserializeObject<List<GetFeedComments_Result>>(result);
-        }
-        public List<GetFeedTags_Result> GetFeedTags(int feedId)
-        {
-            var client = new HttpClient();
-            client.BaseAddress = new Uri(url);
-            var response = client.GetAsync("GetFeedTags/" + feedId).Result;
-            var result = response.Content.ReadAsStringAsync().Result;
-            return JsonConvert.DeserializeObject<List<GetFeedTags_Result>>(result);
+            var response = await client.GetStringAsync("GetHumanInfoByUsername/" + username).ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<GetHumanInformationByUsername_Result>(response);
         }
 
-        public int PublishFeed(int usrId, string text, string filepath, int prioId)
+        /// <summary>
+        /// Get comments that are attached to a feed
+        /// </summary>
+        /// <param name="feedId"></param>
+        /// <returns></returns>
+        public async  Task<List<GetFeedComments_Result>> GetFeedComments(int feedId)
         {
             var client = new HttpClient();
             client.BaseAddress = new Uri(url);
-            var response = client.GetAsync("PostFeed/" + usrId.ToString() + "&" + text + "&" + filepath + "&" + prioId.ToString()).Result;
-            var result = response.Content.ReadAsStringAsync().Result;
-            var obj =  JsonConvert.DeserializeObject<string>(result);
+            var response = await client.GetStringAsync("GetFeedComments/" + feedId).ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<List<GetFeedComments_Result>>(response);
+        }
+
+        /// <summary>
+        /// Get tags that are attached to a feed
+        /// </summary>
+        /// <param name="feedId"></param>
+        /// <returns></returns>
+        public async Task<List<GetFeedTags_Result>> GetFeedTags(int feedId)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            var response = await client.GetStringAsync("GetFeedTags/" + feedId).ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<List<GetFeedTags_Result>>(response);
+        }
+
+        /// <summary>
+        /// Publish a feed to the system
+        /// </summary>
+        /// <param name="usrId"></param>
+        /// <param name="text"></param>
+        /// <param name="filepath"></param>
+        /// <param name="prioId"></param>
+        /// <returns></returns>
+        public async Task<int> PublishFeed(int usrId, string text, string filepath, int prioId)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            var response = await client.GetStringAsync("PostFeed/" + usrId.ToString() + "&" + text + "&" + filepath + "&" + prioId.ToString()).ConfigureAwait(false);
+            var obj =  JsonConvert.DeserializeObject<string>(response);
             return int.Parse(obj);
         }
 
