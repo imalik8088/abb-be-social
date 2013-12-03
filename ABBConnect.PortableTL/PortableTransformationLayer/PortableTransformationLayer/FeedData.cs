3e58d69c88621fa30b48cb080550e3d6edf2e562
simPm <dimitros.23@gmail.com>
2013-12-03 10:03:13 +0100

    Re-addition of Portable TL

---
+++ ABBConnect.PortableTL/PortableTransformationLayer/PortableTransformationLayer/FeedData.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PortableTransformationLayer.ABBConnectServiceRef;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+
+namespace PortableTransformationLayer
+{
+    class FeedData: IFeedData
+    {
+        public async Task<List<ABBConnectServiceRef.GetLatestXFeeds_Result>> GetLatestXFeeds(int X)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            var response = await client.GetStringAsync("GetLatestXFeeds/" + X.ToString()).ConfigureAwait(false);
+
+            //   var result =  response.Content.ReadAsStringAsync().Result;
+            List<GetLatestXFeeds_Result> ret = JsonConvert.DeserializeObject<List<GetLatestXFeeds_Result>>(response);
+
+            // Task<List<GetLatestXFeeds_Result>> namn = ret;
+            return ret;
+        }
+
+        public async Task<List<ABBConnectServiceRef.GetLatestXFeeds_Result>> GetLatestXFeedsFromId(int X, int Id)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            var response = await client.GetStringAsync("GetLatestXFeeds/" + X.ToString() + "&" + Id.ToString()).ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<List<GetLatestXFeeds_Result>>(response); 
+        }
+
+        public async Task<List<ABBConnectServiceRef.GetFeedComments_Result>> GetFeedComments(int feedId)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            var response = await client.GetStringAsync("GetFeedComments/" + feedId).ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<List<GetFeedComments_Result>>(response);
+        }
+
+        public async Task<List<ABBConnectServiceRef.GetFeedTags_Result>> GetFeedTags(int feedId)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            var response = await client.GetStringAsync("GetFeedTags/" + feedId).ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<List<GetFeedTags_Result>>(response);
+        }
+
+        public async Task<int> PublishFeed(int usrId, string text, string filepath, int prioId)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            var response = await client.GetStringAsync("PostFeed/" + usrId.ToString() + "&" + text + "&" + filepath + "&" + prioId.ToString()).ConfigureAwait(false);
+            var obj = JsonConvert.DeserializeObject<string>(response);
+            return int.Parse(obj);
+        }
+    }
+}
---
+++ ABBConnect.PortableTL/PortableTransformationLayer/PortableTransformationLayer/IHumanData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PortableTransformationLayer.ABBConnectServiceRef;
+using System.Threading.Tasks;
+
+namespace PortableTransformationLayer
+{
+    interface IHumanData
+    {
+        Task<bool> LogIn(string usrName, string pw);
+        Task<GetHumanInformation_Result> GetHumanInformation(int Id);
+        Task<GetHumanInformationByUsername_Result> GetHumanInformationByUserName(string username);
+    }
+}
