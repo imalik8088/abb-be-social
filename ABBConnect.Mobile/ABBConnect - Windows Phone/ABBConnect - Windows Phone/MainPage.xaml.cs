2f42095e134e05533e72c74cd7279fb29dd916eb
Robert Gustavsson <rgn09003@student.mdh.se>
2013-12-11 16:58:54 +0100

    added some exception handling

--- ABBConnect.Mobile/ABBConnect - Windows Phone/ABBConnect - Windows Phone/MainPage.xaml.cs
+++ ABBConnect.Mobile/ABBConnect - Windows Phone/ABBConnect - Windows Phone/MainPage.xaml.cs
@@ -127,8 +127,6 @@ namespace ABBConnect___Windows_Phone
 
             if (NoCache > 5)
                 NoCache = 1;
-
-            latestFeed.Clear();
         }
 
         /// <summary>
@@ -136,10 +134,18 @@ namespace ABBConnect___Windows_Phone
         /// </summary>
         private async void LoadUser()
         {
-            PortableBLL.UserManager um = new PortableBLL.UserManager();
-            currentUser = await um.LoadHumanInformationByUsername("rgn09003");
+            try
+            {
+                PortableBLL.UserManager um = new PortableBLL.UserManager();
+                currentUser = await um.LoadHumanInformationByUsername("rgn09003");
 
-            App.CurrentUser = currentUser;
+                App.CurrentUser = currentUser;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
             /*
             lblEmailClick.Text = currentUser.Email;
             lblPhoneClick.Text = currentUser.PhoneNumber;
@@ -157,11 +163,24 @@ namespace ABBConnect___Windows_Phone
         {
             pgbLoadFeed.Visibility = System.Windows.Visibility.Visible;
 
-            feeds = await fm.LoadFeedsByType(currentFeedType, amount);
-            //feeds = await fm.LoadLatestXFeeds(amount);
-            AddFeedsToList(feeds);
+            try
+            {
+                feeds = await fm.LoadFeedsByType(currentFeedType, amount);
 
-            CreateButton(feeds[feeds.Count - 1].ID);
+                if (feeds.Count == 0)
+                {
+                    MessageBox.Show("No feeds loaded");
+                    return;
+                }
+
+                AddFeedsToList(feeds);
+                CreateButton(feeds[feeds.Count - 1].ID);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
 
             pgbLoadFeed.Visibility = System.Windows.Visibility.Collapsed;
         }
@@ -514,6 +533,9 @@ namespace ABBConnect___Windows_Phone
 
         private void OnRefresh(object sender, EventArgs e)
         {
+            if (!ini)
+                return;
+
             pgbLoadFeed.Visibility = System.Windows.Visibility.Visible;
             timerNewFeed.Stop();
             CheckNewFeeds();
