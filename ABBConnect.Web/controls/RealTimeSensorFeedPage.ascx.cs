f7e833a2154faca2ac9fd6194f11b2f0936b74f6
Mario Milas <mylemim@gmail.com>
2013-12-18 15:25:34 +0100

    Minor commit -real time sensor feed page added

---
+++ ABBConnect.Web/controls/RealTimeSensorFeedPage.ascx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using BLL;
+
+public partial class controls_RealTimeSensorFeedPage : System.Web.UI.UserControl
+{
+    // Default Feed Settings
+    public int PageSize = 10;
+    public int LastFeedId = 0;
+    public bool IsLoadMoreVisible = true;
+
+    public DateTime FilterStartDateValue;
+    public DateTime FilterEndDateValue;
+    public int FilterUserId = -1;
+    public string FilterLocation = null;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    public void RenderFeedPage()
+    {        
+        FeedManager feedManager = new FeedManager();       
+       
+        List<Feed> singlePageFeeds = new List<Feed>();
+        singlePageFeeds = feedManager.LoadFeedsByFilter(this.FilterUserId, this.FilterLocation, this.FilterStartDateValue, this.FilterEndDateValue, FeedType.FeedSource.Sensor, LastFeedId, this.PageSize);
+      
+        RealTimeSensorFeedRepeater.DataSource = singlePageFeeds;
+        RealTimeSensorFeedRepeater.DataBind();
+    }
+
+    protected void RealTimeSensorFeedRepeater_ItemDataBound(object sender, RepeaterItemEventArgs e)
+    {
+        Literal l = new Literal();  
+        HtmlAnchor a = new HtmlAnchor();
+        HtmlGenericControl hgc = new HtmlGenericControl();
+        if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+        {
+            /* Determine feedType (Human, Sensor) and render innerMessage (FeedContent) for it (TextMessage, SensorWarning, SensorGauge)
+             * 
+             */
+
+            Feed currentFeed = ((Feed)e.Item.DataItem);
+
+            l = (Literal)e.Item.FindControl("litFeedPosterName");
+            l.Text = "Sensor";
+
+            l = (Literal)e.Item.FindControl("litFeedContent");
+            l.Text = currentFeed.Content;
+       
+            // Render LoadMore Link
+            a = (HtmlAnchor)this.FindControl("load_more");
+            a.Attributes.Add("onclick", "$(this).fadeOut(300); AjaxLoadMoreRealTimeSensorFeeds(" + currentFeed.ID + ")");
+        }
+    }
+    protected override void Render(HtmlTextWriter writer)
+    {
+        if (RealTimeSensorFeedRepeater.Items.Count >= this.PageSize)
+        {
+            feed_page_load_more_container.Visible = true;
+        }
+        else
+        {
+            feed_page_load_more_container.Visible = false;
+        }
+        if (IsLoadMoreVisible == false) feed_page_load_more_container.Visible = false;
+        RenderChildren(writer);
+    }
+}
